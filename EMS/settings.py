33f31380efb5e24ecefd4d50939edf039c678c6b
SumitK3588 <s398271@students.cdu.edu.au>
2026-05-19 09:39:24 +1000

    Update DATABASES setting for environment configuration
    Modify DATABASES setting to use different paths based on the RENDER environment variable.

--- EMS/settings.py
+++ EMS/settings.py
@@ -75,14 +75,20 @@ WSGI_APPLICATION = "EMS.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if os.environ.get('RENDER'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': '/tmp/db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
