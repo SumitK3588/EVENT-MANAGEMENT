bdddf9d84efc90f638b4ec93f7685dc37d45f903
SumitK3588 <s398271@students.cdu.edu.au>
2026-05-19 09:45:13 +1000

    Refactor database settings in settings.py
    Simplified database configuration by removing conditional rendering for the database settings.

--- EMS/settings.py
+++ EMS/settings.py
@@ -75,18 +75,10 @@ WSGI_APPLICATION = "EMS.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if os.environ.get('RENDER'):
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': '/tmp/db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
